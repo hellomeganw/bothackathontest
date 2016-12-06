a5e9d98207c1e900026ad2487e3776692e78d8ff
Megan O'Neill <megan@hellomegan.com>
2016-12-06 15:14:59 -0800

    Update app.js

--- app.js
+++ app.js
@@ -497,24 +497,6 @@ function sendVideoMessage(recipientId) {
   callSendAPI(messageData);
 }
 
-
-/*
-*Testing setting greeting message
-*
-*/
-curl -X POST -H "Content-Type: application/json" -d '{
-  "setting_type":"greeting",
-  "greeting":{
-    "text":"Hi {{user_first_name}}, welcome to this bot."
-  }
-}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=PAGE_ACCESS_TOKEN" 
-}
-}
-}
-};
-
-
-
 /*
  * Send a file using the Send API.
  *
